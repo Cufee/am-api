632db17e152c2ce16ce8bdbc97635b74c77fa66f
Cufee <vkouzin@outlook.com>
2020-10-14 17:25:02 -0700

    logging

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	h "github.com/cufee/am-api/handlers"
 
 	"github.com/gofiber/fiber/v2"
@@ -13,5 +15,5 @@ func main() {
 	app.Get("/login/redirect/:intentID", h.HandleWargamingRedirect)
 	app.Get("/login", h.HandleWargamingLogin)
 
-	app.Listen(":80")
+	log.Print(app.Listen(":80"))
 }
