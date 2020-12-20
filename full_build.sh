19b85f8ccfafa857acdcdd72bd34cc9794fd5c3b
Cufee <vkouzin@outlook.com>
2020-12-20 07:38:26 -0800

    Added restart unless stopped

--- full_build.sh
+++ full_build.sh
@@ -8,4 +8,4 @@ export PATH=$PATH:/usr/local/go/bin
 go build -o build/app
 
 sudo docker build -t cufee/am-api-dev .
-sudo docker run -d --name am-api-dev -p 4001:4000 cufee/am-api-dev:latest
+sudo docker run -d --restart unless-stopped --name am-api-dev -p 4001:4000 cufee/am-api-dev:latest
