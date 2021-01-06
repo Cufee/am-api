62d8bfb0d19a37f73daad9e4ae8f5e3abbd94dd5
Vovko <vkouzin@outlook.com>
2021-01-06 18:51:18 -0500

    changed build script

--- full_build.sh
+++ full_build.sh
@@ -1,11 +1,11 @@
 clear
 git pull
 
-sudo docker stop am-api-dev
-sudo docker rm am-api-dev
+sudo docker stop am-api
+sudo docker rm am-api
 
 export PATH=$PATH:/usr/local/go/bin
 go build -o build/app
 
-sudo docker build -t cufee/am-api-dev .
-sudo docker run -d --restart unless-stopped --name am-api-dev -p 4001:4000 cufee/am-api-dev:latest
+sudo docker build -t cufee/am-api .
+sudo docker run -d --restart unless-stopped --name am-api -p 4000:4000 cufee/am-api:latest
