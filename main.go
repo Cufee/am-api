ef5eed6d7a74d789d5bca96bafa1dfb40def0709
Vovko <vkouzin@outlook.com>
2021-01-18 15:38:30 -0500

    disabled API key for some routes

--- main.go
+++ main.go
@@ -17,8 +17,22 @@ func main() {
 	// Logger
 	app.Use(logger.New())
 
-	// Generate API key
+	// Auth disabled
+
+	// Generate API key - localhost only
 	app.Get("/keys/new", auth.GenerateKey)
+
+	// Root
+	app.Get("/", func(ctx *fiber.Ctx) error { return ctx.Redirect("https://aftermath.link", 301) }) // Root redirect
+
+	// WG redirect
+	app.Get("/redirect/:intentID", h.HandleWargamingRedirect) // Redirect from WG
+
+	// Payments
+	app.Get("/payments/redirect", func(ctx *fiber.Ctx) error { return ctx.Redirect("https://aftermath.link", 301) }) // PayPal redirect
+	app.Post("/payments/events", paypal.HandlePaymentEvent)                                                          // PayPal Events
+
+	// Auth enabled
 
 	// API key validator
 	app.Use(auth.Validator)
@@ -28,26 +42,21 @@ func main() {
 	app.Get("/referrals/:refID", h.HandleReferralLink)
 
 	// WG login routes
-	app.Get("/redirect/:intentID", h.HandleWargamingRedirect)
-	app.Get("/login/:intentID", h.HandleWargamingLogin)
-	app.Get("/newlogin", h.HandleWargamingNewLogin)
+	app.Get("/newlogin", h.HandleWargamingNewLogin)     // New login intent
+	app.Get("/login/:intentID", h.HandleWargamingLogin) // Login using intentID
 
-	// Checks
-	app.Get("/users/:discordID", h.HandeleUserCheck) // Will be dropped
-	app.Get("/users/id/:discordID", h.HandeleUserCheck)
+	// Users
+	app.Get("/users/id/:discordID", h.HandeleUserCheck)                    // Check
+	app.Post("/users/id/:discordID/ban", h.HandleNewBan)                   // Ban
+	app.Patch("/users/:discordID/newdef/:playerID", h.HandleNewDefaultPID) // New default PID
 
-	// Bans
-	app.Post("/users/id/:discordID/ban", h.HandleNewBan)
-
-	app.Patch("/users/:discordID/newdef/:playerID", h.HandleNewDefaultPID)
-
-	app.Get("/players/:playerID", h.HandelePlayerCheckByID) // Will be dropped
-	app.Get("/players/id/:playerID", h.HandelePlayerCheckByID)
-	app.Get("/players/name/:nickname", h.HandelePlayerCheckByName)
+	// Players
+	app.Get("/players/id/:playerID", h.HandelePlayerCheckByID)     // Check by ID
+	app.Get("/players/name/:nickname", h.HandelePlayerCheckByName) // Check by name
 
 	// Backgrounds
-	app.Patch("/background/:discordID", h.HandleSetNewBG)
-	app.Delete("/background/:discordID", h.HandleRemoveBG)
+	app.Patch("/background/:discordID", h.HandleSetNewBG)  // Set new
+	app.Delete("/background/:discordID", h.HandleRemoveBG) // Delete
 
 	// Premium
 	app.Get("/premium/add", h.HandleNewPremiumIntent)
@@ -56,11 +65,6 @@ func main() {
 
 	// Payments
 	app.Get("/payments/new/:discordID", paypal.HandleNewSub)
-	app.Get("/payments/redirect", func(ctx *fiber.Ctx) error { return ctx.Redirect("https://aftermath.link", 301) })
-	app.Post("/payments/events", paypal.HandlePaymentEvent)
-
-	// Root
-	app.Get("/", func(ctx *fiber.Ctx) error { return ctx.Redirect("https://aftermath.link", 301) })
 
 	log.Print(app.Listen(":4000"))
 }
